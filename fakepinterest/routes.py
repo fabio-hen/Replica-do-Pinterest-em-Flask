3ecdff5720a1082c77decbb4532e0cbb9382a6ba
fabio-hen <113725177+fabio-hen@users.noreply.github.com>
2025-03-05 19:19:04 -0300

    Initial commit

---
+++ fakepinterest/routes.py
@@ -0,0 +1,24 @@
+# crias as rotas do site
+
+from flask import render_template, url_for
+from fakepinterest import app
+from flask_login import login_required
+from fakepinterest.forms import FormLogin, FormCriarConta
+
+
+@app.route("/", methods=["GET", "POST"])
+def homepage():
+    formlogin = FormLogin()
+    return render_template("homepage.html", form=formlogin)
+
+
+@app.route("/criarconta", methods=["GET", "POST"])
+def criarconta():
+    formcriarconta = FormCriarConta()
+    return render_template("criarconta.html", form=formcriarconta)
+
+
+@app.route("/perfil/<usuario>")
+@login_required
+def perfil(usuario):
+    return render_template("perfil.html", usuario=usuario)
---
+++ main.py
@@ -0,0 +1,4 @@
+from fakepinterest import app
+
+if __name__ == "__main__":  # se esse arquivo esta executando ele mesmo
+    app.run(debug=True)
